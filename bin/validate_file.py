168279319444a67f4d79720aa56d17f9b4949af0
Iestyn Pryce <iestyn.pryce@gmail.com>
2015-06-14 21:45:47 +0100

    Added validation that all options are set to validate_file.py runner script

--- bin/validate_file.py
+++ bin/validate_file.py
@@ -4,8 +4,8 @@ import argparse
 import validator.validator as vv
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--config', help="configuration file for file validator")
-parser.add_argument('--file', help="file to validate")
+parser.add_argument('--config', help="configuration file for file validator", required=True)
+parser.add_argument('--file', help="file to validate", required=True)
 args = parser.parse_args()
 
 v = vv.Validate("validator.pickle",diefast=False)
